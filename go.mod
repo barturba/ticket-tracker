81a411257fd373373ed8f2f1e1b89c3eef890fa3
barturba <b@bartas.co>
2024-09-01 18:20:33 -0600

    chore: Add godotenv package for environment variable management

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/barturba/ticket-tracker
 
 go 1.22.5
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Couldn't load .env file")
+	}
+
 	fmt.Printf("ticket-tracker\n")
 	os.Exit(0)
 }
