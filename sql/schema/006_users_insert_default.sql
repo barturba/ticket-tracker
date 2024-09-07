ce5bfa71fa777a4850e94b2f92493a36f98a28ca
barturba <b@bartas.co>
2024-09-07 17:59:53 -0600

    feat: Updated default organization name to database schema

--- sql/schema/006_users_insert_default.sql
+++ sql/schema/006_users_insert_default.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 INSERT INTO ORGANIZATIONS (id, created_at, updated_at, name)
-VALUES ('99ae48fb-89e8-4f7b-bd4c-b947b8fe8187', '2024-09-06 00:00:00', '2024-09-06 00:00:00', 'admin');
+VALUES ('99ae48fb-89e8-4f7b-bd4c-b947b8fe8187', '2024-09-06 00:00:00', '2024-09-06 00:00:00', 'admin organization');
 INSERT INTO USERS (id, created_at, updated_at, name, organization_id)
 VALUES ('5faba39f-64fe-4805-8365-0a91bb396477', '2024-09-06 00:00:00', '2024-09-06 00:00:00', 'admin', '99ae48fb-89e8-4f7b-bd4c-b947b8fe8187');
 
