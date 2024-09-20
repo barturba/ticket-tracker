4e0fc77d49ecb0a9d39dbd9984c643c33e4c8c7a
barturba <b@bartas.co>
2024-09-19 18:41:43 -0600

    Refactor handleIndexPage function to remove unnecessary code and improve flow

--- internal/server/handle_index_page.go
+++ internal/server/handle_index_page.go
@@ -8,10 +8,6 @@ import (
 	"github.com/barturba/ticket-tracker/views"
 )
 
-func (cfg *ApiConfig) handleIndexPage(w http.ResponseWriter, r *http.Request, u database.User) {
-	w.Header().Set("HX-Redirect", "/incidents")
-	http.Redirect(w, r, "/incidents", http.StatusFound)
-}
 func (cfg *ApiConfig) handlePageIndex(w http.ResponseWriter, r *http.Request, u database.User) {
 	fromProtected := false
 	if (u != database.User{}) {
