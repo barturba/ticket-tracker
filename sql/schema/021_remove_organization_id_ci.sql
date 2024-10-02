f6995fedddcaaedccda63a721314eefd087fbe22
barturba <b@bartas.co>
2024-10-02 15:55:52 -0600

    Remove organization_id column from configuration_items table

---
+++ sql/schema/021_remove_organization_id_ci.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+
+ALTER TABLE configuration_items DROP COLUMN organization_id;
+
+-- +goose Down
+
+ALTER TABLE configuration_items 
+ADD COLUMN organization_id UUID NOT NULL;
+
+ALTER TABLE configuration_items 
+    ADD CONSTRAINT fk_organizations
+    FOREIGN KEY (organization_id)
+    REFERENCES organizations(id)
+    ON DELETE CASCADE;
