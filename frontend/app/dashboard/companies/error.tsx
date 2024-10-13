e5632ee7633d3abf2ad53e1dac9e24a0293b70f8
barturba <b@bartas.co>
2024-10-12 18:05:45 -0600

    Abstracted common components

--- frontend/app/dashboard/companies/error.tsx
+++ frontend/app/dashboard/companies/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ErrorTemplate from "@/app/ui/utils/error";
 import { useEffect } from "react";
 
 export default function Error({
@@ -10,21 +11,7 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Optionally log this to an external logging service
     console.error(error);
   }, [error]);
-  return (
-    <main className="flex h-full flex-col items-center justify-center">
-      <h2 className="text-center">Something went wrong!</h2>
-      <button
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={
-          // Attempt to recover by trying to re-render the companies route
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </main>
-  );
+  return <ErrorTemplate reset={reset} />;
 }
