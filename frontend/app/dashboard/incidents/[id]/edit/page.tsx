69f28c9cc843684233c3e82ee8666034c1b43e75
barturba <b@bartas.co>
2024-10-13 19:37:24 -0600

    Cleaned up incident edit page even more

--- frontend/app/dashboard/incidents/[id]/edit/page.tsx
+++ frontend/app/dashboard/incidents/[id]/edit/page.tsx
@@ -1,23 +1,16 @@
-import Breadcrumbs from "@/app/ui/utils/breadcrumbs";
 import EditForm from "@/app/ui/incidents/edit-form";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { getCIs, getCIsAll } from "@/app/lib/actions/cis";
-import { getCompanies, getCompaniesAll } from "@/app/lib/actions/companies";
+import { getCIsAll } from "@/app/lib/actions/cis";
+import { getCompaniesAll } from "@/app/lib/actions/companies";
 import { getIncident } from "@/app/lib/actions/incidents";
-import { getUsers, getUsersAll } from "@/app/lib/actions/users";
-import { Heading, Subheading } from "@/app/components/heading";
-import { Badge } from "@/app/components/badge";
-import { BanknotesIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
+import { getUsersAll } from "@/app/lib/actions/users";
 import HeadingEdit from "@/app/application-components/heading-edit";
 import HeadingSubEdit from "@/app/application-components/heading-sub-edit";
 
-export async function generateMetadata(
-  props: {
-    params: Promise<{ id: string }>;
-  }
-): Promise<Metadata> {
+export async function generateMetadata(props: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
   const params = await props.params;
   let incident = await getIncident(params.id);
 
@@ -25,7 +18,9 @@ export async function generateMetadata(
     title: incident && `Incident #${incident.id}`,
   };
 }
-export default async function Incident(props: { params: Promise<{ id: string }> }) {
+export default async function Incident(props: {
+  params: Promise<{ id: string }>;
+}) {
   const params = await props.params;
   const id = params.id;
   const [incident, usersData, companiesData, cisData] = await Promise.all([
