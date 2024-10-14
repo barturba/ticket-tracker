78b7b117aaf6bfd7c62997947f811bf0d3ea61b6
barturba <b@bartas.co>
2024-10-14 11:49:33 -0600

    Fixed homepage layout.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
-import Link from "next/link";
+import { Heading } from "./components/heading";
+import { Button } from "./components/button";
 
 export const metadata: Metadata = {
   title: "Ticket Tracker",
@@ -7,20 +8,13 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="bg-white">
-      <div className="px-6 py-24 mx-auto max-w-7xl sm:py-32 lg:px-8">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Ticket Tracker
-        </h2>
-        <div className="flex items-center mt-10 gap-x-6">
-          <Link
-            href="/login"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Log in
-          </Link>
-        </div>
+    <>
+      <Heading>Welcome to Ticket Tracker</Heading>
+      <div className="mt-8 flex items-end justify-between">
+        <Button className="-my-0.5" href="/dashboard">
+          Dashboard
+        </Button>
       </div>
-    </div>
+    </>
   );
 }
