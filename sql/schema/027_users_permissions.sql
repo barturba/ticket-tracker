b1bb9343ec3957624c2ee76b75391f011e90bc64
barturba <b@bartas.co>
2024-10-19 13:33:42 -0600

    Refactor users_permissions table schema and add initial data

--- sql/schema/027_users_permissions.sql
+++ sql/schema/027_users_permissions.sql
@@ -9,11 +9,19 @@ CREATE TABLE IF NOT EXISTS permissions (
 );
 
 CREATE TABLE IF NOT EXISTS users_permissions (
-    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE
-    permission_id uuid NOT NULL,
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    permission_id uuid NOT NULL REFERENCES permissions(id) ON DELETE CASCADE,
 
+    PRIMARY KEY (user_id, permission_id)
 );
--- +goose Down
 
-ALTER TABLE users
-DROP COLUMN role;
+INSERT INTO permissions (code) 
+VALUES ('users:read'), ('users:write'), ('users:delete'),
+('cis:read'), ('cis:write'), ('cis:delete'),
+('companies:read'), ('companies:write'), ('companies:delete'),
+('permissions:read'), ('permissions:write'), ('permissions:delete'),
+('users_permissions:read'), ('users_permissions:write'), ('users_permissions:delete');
+
+-- +goose Down
+DROP TABLE IF EXISTS users_permissions;
+DROP TABLE IF EXISTS permissions;
